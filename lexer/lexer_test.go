f7042b1588acd2d4faea657b2c32b246be221898
Stephanie Gredell <s.raide@gmail.com>
2024-05-28 01:44:02 -0700

    add support for true, false if else and return... among other things

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -15,9 +15,15 @@ let add = fn(x, y) {
 
 let result = add(five, ten);
 !-/*5;
-5 < 10 > 5;
-`
 
+5 < 10 > 5;
+if (5 < 10) {
+	return true;
+} else {
+	return false;
+}`
+
+	// this should match the input above
 	tests := []struct {
 		expectedType    token.TokenType
 		expectedLiteral string
@@ -64,13 +70,30 @@ let result = add(five, ten);
 		{token.ASTERISK, "*"},
 		{token.INT, "5"},
 		{token.SEMICOLON, ";"},
+
 		{token.INT, "5"},
 		{token.LT, "<"},
 		{token.INT, "10"},
 		{token.GT, ">"},
 		{token.INT, "5"},
 		{token.SEMICOLON, ";"},
-		{token.EOF, ""},
+		{token.IF, "if"},
+		{token.LPAREN, "("},
+		{token.INT, "5"},
+		{token.LT, "<"},
+		{token.INT, "10"},
+		{token.RPAREN, ")"},
+		{token.LBRACE, "{"},
+		{token.RETURN, "return"},
+		{token.TRUE, "true"},
+		{token.SEMICOLON, ";"},
+		{token.RBRACE, "}"},
+		{token.ELSE, "else"},
+		{token.LBRACE, "{"},
+		{token.RETURN, "return"},
+		{token.FALSE, "false"},
+		{token.SEMICOLON, ";"},
+		{token.RBRACE, "}"},
 	}
 
 	l := New(input)
--- token/token.go
+++ token/token.go
@@ -39,11 +39,21 @@ const (
 	// Keywords
 	FUNCTION = "FUNCTION"
 	LET      = "LET" // I'm going to change this to var at some point
+	TRUE     = "TRUE"
+	FALSE    = "FALSE"
+	IF       = "IF"
+	ELSE     = "ELSE"
+	RETURN   = "RETURN"
 )
 
 var keywords = map[string]TokenType{
-	"fn":  FUNCTION,
-	"let": LET,
+	"fn":     FUNCTION,
+	"let":    LET,
+	"true":   TRUE,
+	"false":  FALSE,
+	"if":     IF,
+	"else":   ELSE,
+	"return": RETURN,
 }
 
 // checks the keywords table to see whether the given identifier is in fact a keyword
