5a86a3d49c36c0d252058972a2ab7e4ef5730f8e
Stephanie Gredell <s.raide@gmail.com>
2024-06-01 11:05:22 -0700

    Add expression statements and string methods

--- ast/ast.go
+++ ast/ast.go
@@ -1,11 +1,13 @@
 package ast
 
 import (
+	"bytes"
 	"slang/token"
 )
 
 type Node interface {
 	TokenLiteral() string
+	String() string // allows us to print AST nodes for debugging and to compare them with other nodes
 }
 
 type Statement interface {
@@ -31,10 +33,20 @@ func (p *Program) TokenLiteral() string {
 	}
 }
 
+func (p *Program) String() string {
+	var out bytes.Buffer
+
+	for _, s := range p.Statements {
+		out.WriteString(s.String())
+	}
+
+	return out.String()
+}
+
 // bind a value to a given name.
 // var x = 5 -> name = x and value = 5
 type VarStatement struct {
-	Token token.Token
+	Token token.Token // the token.VAR token
 	Name  *Identifier
 	Value Expression
 }
@@ -49,18 +61,65 @@ type ReturnStatement struct {
 	ReturnValue Expression
 }
 
-func (vs *VarStatement) statementNode()    {}
-func (rs *ReturnStatement) statementNode() {}
-func (i *Identifier) expressionNode()      {}
+type ExpressionStatement struct {
+	Token      token.Token
+	Expression Expression
+}
+
+func (vs *VarStatement) statementNode()        {}
+func (rs *ReturnStatement) statementNode()     {}
+func (i *Identifier) expressionNode()          {}
+func (es *ExpressionStatement) statementNode() {}
 
 func (vs *VarStatement) TokenLiteral() string {
 	return vs.Token.Literal
+}
+
+func (vs *VarStatement) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(vs.TokenLiteral() + " ")
+	out.WriteString(vs.Name.String())
+	out.WriteString(" = ")
+
+	if vs.Value != nil {
+		out.WriteString(vs.Value.String())
+	}
+
+	out.WriteString(";")
+
+	return out.String()
 }
 
 func (i *Identifier) TokenLiteral() string {
 	return i.Token.Literal
 }
 
+func (i *Identifier) String() string {
+	return i.Value
+}
+
 func (rs *ReturnStatement) TokenLiteral() string {
 	return rs.Token.Literal
+}
+
+func (rs *ReturnStatement) String() string {
+	var out bytes.Buffer
+	out.WriteString(rs.TokenLiteral() + " ")
+	if rs.ReturnValue != nil {
+		out.WriteString(rs.ReturnValue.String())
+	}
+	out.WriteString(";")
+	return out.String()
+}
+
+func (es *ExpressionStatement) TokenLiteral() string {
+	return es.Token.Literal
+}
+
+func (es *ExpressionStatement) String() string {
+	if es.Expression != nil {
+		return es.Expression.String()
+	}
+	return ""
 }
